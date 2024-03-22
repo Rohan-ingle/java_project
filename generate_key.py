6e371a8a54fcf21518d03940d9d6f29550bceffd
Rohan <rohaningle911@gmail.com>
2024-03-22 16:10:41 +0530

    Updated key generator

--- generate_key.py
+++ generate_key.py
@@ -1,4 +1,8 @@
-from Cryptography import generate_key
+from cryptography.fernet import Fernet
+
+def generate_key():
+    key = Fernet.generate_key()
+    return key
 
 with open('key.txt','wb') as f:
     f.write(generate_key())
